4475c0939fe0575e882acd4bfd50f228f2ebe550
Tomas <tomas.krejzlik@gmail.com>
2019-02-16 17:09:44 +0100

    pridany 2 templates - login, registrace

--- eshop/eshop/urls.py
+++ eshop/eshop/urls.py
@@ -4,6 +4,7 @@ from shop import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login', views.login, name="login"),
     path('registrace', views.registrace, name="registrace"),
     path('', views.index, name="index"),
 
