4f24eb23f94b7b4c0d0c8b71f3a03816d051bd9e
Tomas <tomas.krejzlik@gmail.com>
2019-03-03 19:45:00 +0100

    registrace uzivatele, vytvoreni databaze pro uzivatele

--- eshop/shop/models.py
+++ eshop/shop/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+import json
+json_dec = json.JSONDecoder()
 
-# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=30, default="")
+    password = models.CharField(max_length=300, default="")
+    email = models.EmailField(default="")
+    is_admin = models.BooleanField(default=False)
+
+    def right_user(self, username, password):
+        if username == self.username and self.password == password:
+            return True
+        return False
+
+    def is_registred(self, username):
+        if username == self.username:
+            return True
+        return False
+
+    def __str__(self):
+        return str(self.username)
+
--- eshop/shop/views.py
+++ eshop/shop/views.py
@@ -1,13 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 # Create your views here.
+from shop.models import User
+
 
 def index(request):
-    return render(request, "index.html")
+    return render(request, "index.html", {"users": User.objects.all()})
+
 
 def login(request):
     return render(request, "login.html")
 
 def registrace(request):
-    return render(request, "registrace.html")
+    if request.method == "GET":
+        return render(request, "registrace.html")
+    elif request.method == "POST":
+        password = request.POST.get("pass1", "")
+        username = request.POST.get("username", "")
+        email = request.POST.get("email", "")
+        if username and password:
+            if not User.objects.filter(username=username):
+                user = User(username=username, password=password,email=email)
+                user.save()
+                request.session["username"] = username
+                return redirect("/")
+        return render(request, "registrace.html",{"error": "Tento uživatel již existuje!"})
+
+def login(request):
+    if request.method == "GET":
+        return render(request, "login.html")
+    elif request.method == "POST":
+        password = request.POST.get("pass1", "")
+        username = request.POST.get("username", "")
+        if User.objects.filter(username=username, password=password):
+            request.session["username"] = username
+            return redirect("/")
+        return render(request, "login.html", {"error2": "Bad username or password!"})
+
+
+
